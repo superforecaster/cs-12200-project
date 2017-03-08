6075fe71d49932ae67dd9b0dfe3506821ae5b1a4
Super Forecasters <armengol@uchicago.edu>
2017-03-08 17:22:50 -0600

    Add files via upload

---
+++ querys.sql
@@ -0,0 +1,16 @@
+--MINI BASE QUERIES
+
+SELECT * FROM crime c
+JOIN economic_activity ea ON ea.year = c.year AND ea.state_key = c.state_key
+JOIN employment e ON c.year = e.year AND c.state_key = e.state_key 
+JOIN urbanization u ON c.year = u.year AND c.state_key = u.state_key
+JOIN justice_system js ON c.year = js.year AND c.state_key = js.state_key
+JOIN cars ca ON c.year = ca.year AND c.state_key = ca.state_key
+JOIN strikes s ON c.year = s.year AND c.state_key = s.state_key;
+
+
+
+
+
+
+ 
