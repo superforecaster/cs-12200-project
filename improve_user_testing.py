a028a4e494b555580ac32d84a88b7f41ce1988a6
Faraz Ahmed <faraz.s.ahmad@gmail.com>
2017-03-09 23:55:14 -0600

    Add files via upload

--- improve_user_testing.py
+++ improve_user_testing.py
@@ -7,7 +7,7 @@ import pandas as pd
 import seaborn as sns
 import io
 from io import BytesIO
-
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import base64
@@ -17,17 +17,16 @@ app = Flask(__name__)
 app.jinja_loader = jinja2.FileSystemLoader('templates/')
 
 DATA_DIR = os.path.dirname(__file__)
-DATABASE_FILENAME = os.path.join(DATA_DIR, 'mini_base.db')
 
+'''DATABASE 1'''
+DATABASE_FILENAME = os.path.join(DATA_DIR, 'mini_base.db')
 connection = sqlite3.connect(DATABASE_FILENAME) 
 cursor_object = connection.cursor()
 
-'''2'''
+'''DATABASE 2'''
 DATABASE_FILENAME_2 = os.path.join(DATA_DIR, 'crime1.db')
-
 connection_2 = sqlite3.connect(DATABASE_FILENAME_2) 
 cursor_object_2 = connection_2.cursor()
-
 
 
 @app.route('/')
@@ -72,6 +71,7 @@ def year_wise_options():
 
     return render_template('post.html', years = years)
 
+
 @app.route('/state_wise_options')
 def state_wise_options():
 
@@ -85,19 +85,24 @@ def state_wise_options():
 @app.route('/year_state_both_wise_options')
 def year_state_both_wise_options():
 
-    sql_querry = "SELECT DISTINCT state_name from crime;"
+    sql_querry = "SELECT DISTINCT state_name from crime"
 
     states = sql_options_reader(sql_querry, cursor_object_2)
+    print(states)
 
-    sql_querry_2 = "SELECT DISTINCT year from crime;"
+    del states[0]
+
+
+    sql_querry_2 = "SELECT DISTINCT year from crime"
 
     years = sql_options_reader(sql_querry_2, cursor_object_2)
 
+    del years[0]
+
     return render_template('year_state_both_wise_options.html', states=states, years=years)
 
+
 @app.route('/corr_matrix_decision_options', methods = ['GET'])
-
-
 def corr_matrix_decision_options():
     return render_template('corr_matrix_options.html')
 
@@ -128,23 +133,18 @@ def corr_matrix_options():
         return render_template('corr_matrix_year.html', years=years)
 
 
-
 @app.route('/predictive_analytics_options')
 def predictive_analytics_options():
 
-    print("BAAA")
+    crime_types_list = ['total','violent','homicides','household_crimes','rape','general_homicides',\
+    'injuries','other_crimes','kidnap','common_thief','cattle_thief',\
+    'banking_thief','highway_thief','no_violent']
 
-    sql_querry = "SELECT DISTINCT state_name from crime;"
+    sql_querry = "SELECT DISTINCT year from crime where year >= 2008 and year <= 2011;"
 
-    states = sql_options_reader(sql_querry, cursor_object)
+    years = sql_options_reader(sql_querry, cursor_object)
 
-    sql_querry_2 = "SELECT DISTINCT year from crime where year >= 2008 and year <= 2011;"
-
-    years = sql_options_reader(sql_querry_2, cursor_object)
-
-    return render_template('predictive_analytics_options.html', states=states, years=years)
-
-
+    return render_template('predictive_analytics_options.html', crime_types_list=crime_types_list, years=years)
 
 
 '''OUTPUT PAGES'''
@@ -156,24 +156,14 @@ def sql_year_wise():
 
     header = year
 
-    sql_querry = "SELECT state_name, total from crime where year = ? LIMIT 5"
+    sql_querry = "SELECT state_name, total from crime where year = ? LIMIT 7"
     rows, column_1_table1, column_2_table1 = sql_input_reader(sql_querry, [year], cursor_object)
     
-    df = pd.DataFrame( [[ij for ij in i] for i in rows])
+    column_2_table1 = column_2_table1 + '_crime'
 
-    labels = df[0]
-    values = df[1]
-    
-    # for descriptive statistics
     column_1_table2 = "Descriptive Statistics"
-    stats = df.describe()
-    desc_stats = []
 
-    for i in range(len(stats.index)):
-        stats_tuple = (stats.index[i], stats[1][i])
-        desc_stats.append(stats_tuple)
-
-    max_value = max(values)
+    labels, values, desc_stats, max_value = pandas_desc_stats(rows)
 
     return render_template('list.html', rows = rows, labels=labels, values=values, max_value=max_value, header=header, column_1_table1=column_1_table1, column_2_table1=column_2_table1, desc_stats = desc_stats, column_1_table2=column_1_table2)
 
@@ -185,30 +175,22 @@ def sql_state_wise():
 
     header = state_name
 
-    sql_querry = "SELECT year, total from crime where state_name = ? LIMIT 5"
+    sql_querry = "SELECT year, total from crime where state_name = ? LIMIT 7"
     rows, column_1_table1, column_2_table1 = sql_input_reader(sql_querry, [state_name], cursor_object)
-    
-    df = pd.DataFrame( [[ij for ij in i] for i in rows])
 
-    labels = df[0]
-    values = df[1]
-    
-    # for descriptive statistics
+    column_2_table1 = column_2_table1 + "_crime"
+
     column_1_table2 = "Descriptive Statistics"
-    stats = df.describe()
-    desc_stats = []
 
-    for i in range(len(stats.index)):
-        stats_tuple = (stats.index[i], stats[1][i])
-        desc_stats.append(stats_tuple)
-
-    max_value = max(values)
+    labels, values, desc_stats, max_value = pandas_desc_stats(rows)
 
     return render_template('list_2.html', rows = rows, labels=labels, values=values, max_value=max_value, header=header, column_1_table1=column_1_table1, column_2_table1=column_2_table1, desc_stats = desc_stats, column_1_table2=column_1_table2)
 
 
 @app.route('/year_state_both_wise_output', methods = ['GET'])
 def sql_year_state_both():
+
+    print("zdfd")
 
     year = int(request.args['year'])
 
@@ -221,24 +203,13 @@ def sql_year_state_both():
 
     rows, column_1_table1, column_2_table1 = sql_input_reader(sql_querry, [year, state_name], cursor_object_2)
     
-    df = pd.DataFrame( [[ij for ij in i] for i in rows])
+    column_2_table1 = "crime_rate"
 
-    labels = df[0]
-    values = df[1]
-    
-    # for descriptive statistics
     column_1_table2 = "Descriptive Statistics"
-    stats = df.describe()
-    desc_stats = []
 
-    for i in range(len(stats.index)):
-        stats_tuple = (stats.index[i], stats[1][i])
-        desc_stats.append(stats_tuple)
-
-    max_value = max(values)
+    labels, values, desc_stats, max_value = pandas_desc_stats(rows)
 
     return render_template('list.html', rows = rows, labels=labels, values=values, max_value=max_value, header=header, header2=header2, column_1_table1=column_1_table1, column_2_table1=column_2_table1, desc_stats = desc_stats, column_1_table2=column_1_table2)
-
 
 
 
@@ -261,24 +232,7 @@ def sql_corr_matrix_state():
     
     df = pd.DataFrame( [[ij for ij in i] for i in rows])
 
-    #print(df.index)
-
     correlation = df.corr(method = 'pearson')
-    print(correlation)
-
-    labels = df[0]
-    values = df[1]
-    
-    # for descriptive statistics
-    column_1_table2 = "Descriptive Statistics"
-    stats = df.describe()
-    desc_stats = []
-
-    for i in range(len(stats.index)):
-        stats_tuple = (stats.index[i], stats[1][i])
-        desc_stats.append(stats_tuple)
-
-    max_value = max(values)
 
     correlation = correlation.fillna(0)
 
@@ -316,30 +270,12 @@ def sql_corr_matrix_year():
     JOIN justice_system js ON c.year = js.year AND c.state_key = js.state_key \
     where c.year = ?"
 
-    #sql_querry = "SELECT crime_type, total from crime where year = ? and state_name = ?"
-
     rows, column_1_table1, column_2_table1 = sql_input_reader(sql_querry, [year], cursor_object)
     
     df = pd.DataFrame( [[ij for ij in i] for i in rows])
 
-    #print(df.index)
-
     correlation = df.corr(method = 'pearson')
     #print(correlation)
-
-    labels = df[0]
-    values = df[1]
-    
-    # for descriptive statistics
-    column_1_table2 = "Descriptive Statistics"
-    stats = df.describe()
-    desc_stats = []
-
-    for i in range(len(stats.index)):
-        stats_tuple = (stats.index[i], stats[1][i])
-        desc_stats.append(stats_tuple)
-
-    max_value = max(values)
 
     correlation = correlation.fillna(0)
     #print(correlation)
@@ -362,7 +298,6 @@ def sql_corr_matrix_year():
 
     #print(plot_url)
 
-
     cmap = sns.diverging_palette(5, 250, as_cmap=True)
 
     #print(cmap)
@@ -376,14 +311,32 @@ def sql_corr_matrix_year():
     with open('templates/corr_matrix.html', 'w') as html:
         html.write(v.render())
 
-    #'<img src="data:image/png;base64,{}">'.format(plot_url_
-
     return render_template('corr_matrix.html')
 
-'''
-how to make a correlation matrix out of pandas given a year or a state?
-just make correlation matrices (pandas styles + sns plot), given a year or a state
-'''
+
+@app.route('/predictive_analytics_output', methods = ['GET'])
+def sql_predictive_analytics_output():
+
+    year = int(request.args['year'])
+    crime_type = str(request.args['crime_type'])
+    
+    #df with statename and crime rate
+    #pedro_df
+
+    x = pd.DataFrame(np.random.randn(20, 5))
+    y = pd.DataFrame(np.random.randn(5, 50))
+
+    '''
+    html = (x.style \
+          .format(format_numbers) \
+          .set_table_styles(styles) \
+          .set_properties(**{'width': '100', 'margin-left': 'auto', 'margin-right': 'auto', 'margin': '0px auto', "align": "center", "text-align": "center"}) \
+          .set_caption("Actual Data") \
+          .render())
+    '''
+
+    return render_template("predictive_analytics_output.html", data=x.to_html(), data2=y.to_html())
+
 
 '''helper functions'''
 
@@ -401,8 +354,6 @@ def magnify():
 ]
 
 
-
-
 def sql_input_reader(sql_querry, argument, cursor_object):
 
     cursor_execute = cursor_object.execute(sql_querry, argument)    
@@ -415,6 +366,7 @@ def sql_input_reader(sql_querry, argument, cursor_object):
 
     return (rows, column_1_table1, column_2_table1)
 
+
 def sql_options_reader(sql_querry, cursor_object):
     
     cursor_execute = cursor_object.execute(sql_querry)    
@@ -422,6 +374,28 @@ def sql_options_reader(sql_querry, cursor_object):
     variable = cursor_execute.fetchall()
 
     return variable
+
+
+def pandas_desc_stats(rows):
+    df = pd.DataFrame( [[ij for ij in i] for i in rows])
+
+    df = df.fillna(0)
+
+    labels = df[0]
+    values = df[1]
+    
+    # for descriptive statistics
+    
+    stats = df.describe()
+    desc_stats = []
+
+    for i in range(len(stats.index)):
+        stats_tuple = (stats.index[i], stats[1][i])
+        desc_stats.append(stats_tuple)
+
+    max_value = max(values)
+
+    return (labels, values, desc_stats, max_value)
 
 
 # Things to do
@@ -435,6 +409,7 @@ def sql_options_reader(sql_querry, cursor_object):
 # 8) how to label axes in bar charts???
 # 9) increase the size of the chart a little bit for certain options
 # 10) how to display the output in the same page as options page...
+# 11) Change the names from Spanish to English for the crime type
 
 
 
